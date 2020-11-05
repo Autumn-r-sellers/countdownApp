e5ed02ae501982f465daf6bd6072bb17e8ecdbec
Autumn Sellers <auresell1993@gmail.com>
2020-11-04 20:33:55 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper">
+    <div class="header">
+      <h1>Countdown Timer</h1>
+    </div>
+    <div class="headline">
+      <h2>How long to your date?</h2>
+    </div>
+    <button class="create buttonClass">
+      <h3>New Countdown</h3>
+    </button>
+    <button class="view buttonClass">
+      <h3>View Countdowns</h3>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  let active = true;
+
+  methods: {
+    activate: function(){
+      
+    }
+  }
+}
+</script>
