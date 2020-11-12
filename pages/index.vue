93c63753722bc8b3c948b23e1d145a58bcd48ab2
Autumn Sellers <auresell1993@gmail.com>
2020-11-12 09:41:20 -0500

    starting on js

--- pages/index.vue
+++ pages/index.vue
@@ -6,9 +6,12 @@
     <div class="headline">
       <h2>How long to your date?</h2>
     </div>
-    <button class="create buttonClass">
+    <button class="create buttonClass" :click="newCountdown">
       <h3>New Countdown</h3>
     </button>
+    <form id="newCountdown" >
+
+    </form>
     <button class="view buttonClass">
       <h3>View Countdowns</h3>
     </button>
@@ -17,12 +20,11 @@
 
 <script>
 export default {
-  let active = true;
-
+  data: {
+    formA: false
+  }
   methods: {
-    activate: function(){
-      
-    }
+    newCpimnt
   }
 }
 </script>
